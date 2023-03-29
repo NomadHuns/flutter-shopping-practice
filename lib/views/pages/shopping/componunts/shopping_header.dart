8d35d31bb8aba14124273b9c97a8840488eb323b
kimalss22 <kimalss22@gmail.com>
2023-03-29 11:42:24 +0900

    shpping_header 구조 구현 완료

--- lib/views/pages/shopping/componunts/shopping_header.dart
+++ lib/views/pages/shopping/componunts/shopping_header.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ShoppingCartHeader extends StatelessWidget {
@@ -5,6 +6,38 @@ class ShoppingCartHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Image.asset("assets/p1.jpeg"),
+        Row(
+          children: [
+            Container(
+              color: Colors.orange,
+              width: 70,
+              height: 70,
+              child: Icon(CupertinoIcons.airplane),
+            ),
+            Container(
+              color: Colors.orange,
+              width: 70,
+              height: 70,
+              child: Icon(CupertinoIcons.car_detailed),
+            ),
+            Container(
+              color: Colors.orange,
+              width: 70,
+              height: 70,
+              child: Icon(Icons.motorcycle),
+            ),
+            Container(
+              color: Colors.orange,
+              width: 70,
+              height: 70,
+              child: Icon(Icons.directions_bike),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
